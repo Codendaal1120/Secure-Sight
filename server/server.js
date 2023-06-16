91ba8ff430af8aeed7a36efa8d3620165cd8ca16
Christiaan Odendaal <christiaan@oden.co.za>
2023-06-16 11:28:58 +0200

    Cleanup

--- server/server.js
+++ server/server.js
@@ -5,9 +5,6 @@ const app = express();
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const { proxy, scriptUrl } = require('rtsp-relay')(app);
-
-
 
 /************* Setup *************/ 
 dotenv.config({ path: path.resolve(root, '.env.development')});
@@ -24,34 +21,9 @@ app.use(bodyParser.json());
 app.get('/echo', async (req, res) => { 
     res.send("Welcome to Secure Sight");
 });  
-
-// app.get('/test', (req, res) =>
-//   res.send(`
-//   <canvas id='canvas' style="width: 1920px; height: 1080px; display: block"></canvas>
-
-//   <script src='${scriptUrl}'></script>
-//   <script>
-//     loadPlayer({
-//       url: 'ws://' + location.host + '/api/cameras/xxx/stream',
-//       canvas: document.getElementById('canvas')
-//     });
-//   </script>
-// `),
-// );
-
-
-/************* Endpoints *************/ 
-// handle RTSP stream streams
-// app.ws('/api/cameras/:camUrl/stream', proxy({
-//     url: 'rtsp://admin:123456@192.168.86.58:554/stream1',
-//     verbose: true ,
-// }));
-
   
 app.use("/api/health", require("./app/controllers/health"));
 app.use("/api/cameras", require("./app/controllers/cameras"));
-
-
 
 // Start webserver and listen for connections
 app.listen(port, () => {  
@@ -59,5 +31,3 @@ app.listen(port, () => {
 })
 
 module.exports = app;
-
-
