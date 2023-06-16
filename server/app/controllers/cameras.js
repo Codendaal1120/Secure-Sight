e531711cb89b16dfb7488656890490328b1e52ad
Christiaan Odendaal <christiaan@oden.co.za>
2023-06-16 11:28:17 +0200

    #10 | WIP camera stream

--- server/app/controllers/cameras.js
+++ server/app/controllers/cameras.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const camService = require("../services/camerService");
 
+const { proxy, scriptUrl } = require('rtsp-relay')(router);
+
 /**
  * Get all configured cameras
  * @returns {object} Returns list of cameras
@@ -15,5 +17,37 @@ router.get("/", async function (req, res) {
         res.status(500).send(result.error);
     }     
 });
+
+/**
+ * Get a specific stream
+ * @returns {WebSocket} socket
+ */
+router.ws('/:camUrl/stream', (ws, req) =>{
+    // TODO: lookup cam
+    let cam = { name : 'test', url :  'rtsp://admin:123456@192.168.86.58:554/stream1' }
+    return proxy({
+        verbose: true ,
+        transport: 'tcp',
+        url: cam.url,
+      })(ws);
+});
+
+/**
+ * temp endpoint to check stream
+ * @returns {string} HTML page
+ */
+router.get('/test', (req, res) =>
+  res.send(`
+  <canvas id='canvas' style="width: 1920px; height: 1080px; display: block"></canvas>
+
+  <script src='${scriptUrl}'></script>
+  <script>
+    loadPlayer({
+      url: 'ws://' + location.host + '/api/cameras/648811f030e04fc1ff98568d/stream',
+      canvas: document.getElementById('canvas')
+    });
+  </script>
+`),
+);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const { proxy, scriptUrl } = require('rtsp-relay')(app);
 
+
+
 /************* Setup *************/ 
 dotenv.config({ path: path.resolve(root, '.env.development')});
 app.use(cors({
@@ -16,38 +18,40 @@ app.use(cors({
 }));
 
 const port = process.env.PORT; 
-const handler = proxy({
-    url: 'rtsp://admin:admin@192.168.86.50:8554/profile0',
-    verbose: true ,
-});
 
 app.use(bodyParser.json());
-
 /************* Endpoints *************/ 
-// handle RTSP stream streams
-app.ws('/api/stream', handler);
-
-// temp endpoint to check stream
-app.get('/test/stream', (req, res) =>
-  res.send(`
-  <canvas id='canvas' width="500" height="500"></canvas>
-
-  <script src='${scriptUrl}'></script>
-  <script>
-    loadPlayer({
-      url: 'ws://' + location.host + '/api/stream',
-      canvas: document.getElementById('canvas')
-    });
-  </script>
-`),
-);
-
 app.get('/echo', async (req, res) => { 
     res.send("Welcome to Secure Sight");
 });  
+
+// app.get('/test', (req, res) =>
+//   res.send(`
+//   <canvas id='canvas' style="width: 1920px; height: 1080px; display: block"></canvas>
+
+//   <script src='${scriptUrl}'></script>
+//   <script>
+//     loadPlayer({
+//       url: 'ws://' + location.host + '/api/cameras/xxx/stream',
+//       canvas: document.getElementById('canvas')
+//     });
+//   </script>
+// `),
+// );
+
+
+/************* Endpoints *************/ 
+// handle RTSP stream streams
+// app.ws('/api/cameras/:camUrl/stream', proxy({
+//     url: 'rtsp://admin:123456@192.168.86.58:554/stream1',
+//     verbose: true ,
+// }));
+
   
 app.use("/api/health", require("./app/controllers/health"));
 app.use("/api/cameras", require("./app/controllers/cameras"));
+
+
 
 // Start webserver and listen for connections
 app.listen(port, () => {  
