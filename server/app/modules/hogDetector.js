2e5cd553709bf9494f7da47781d9f14a6b2ecdfd
Christiaan Odendaal <christiaan@oden.co.za>
2023-08-06 10:48:53 +0200

    WIP hog

--- server/app/modules/hogDetector.js
+++ server/app/modules/hogDetector.js
@@ -1,5 +1,7 @@
 /*
-HOG feature descriptor works best on colored images.
+  HOG feature descriptor works best on colored images.
+  https://debuggercafe.com/image-recognition-using-histogram-of-oriented-gradients-hog-descriptor/
+  https://trinhngocthuyen.com/posts/tech/a-dive-into-hog/
 */
 const BLOCK_SIZE = 8;
 const SCAN_BLOCK_SIZE = 16;
@@ -17,15 +19,10 @@ const processImage = async function(imageData, imageWidth, imageHeight) {
   let blockCount = 0;
 
   for (let y = 0; y < imageHeight; y += BLOCK_SIZE) {
-    for (let x = 0; x < imageWidth; x += BLOCK_SIZE) {
-
-
-      
+    for (let x = 0; x < imageWidth; x += BLOCK_SIZE) {      
 
       blockCount++;
-      processBlock(imageData, x, y, SCAN_BLOCK_SIZE, imageWidth);
-
-      
+      processBlock(imageData, x, y, SCAN_BLOCK_SIZE, imageWidth);     
 
 
       
@@ -42,15 +39,21 @@ processBlock = function(imageData, blockX, blockY, blockSize, imageWidth){
   let yEnd = blockY + blockSize;
   let pixels = [];
 
-  for (let x = blockX; x <= xEnd; x++) {
-    for (let y = blockY; y <= yEnd; y++) {
-
-      let i = Math.max(0, (imageWidth * 4 * (y - 1)) + (x * 4) - 4);
-      pixels.push(imageData[i]);
-      pixels.push(imageData[i+1]);
-      pixels.push(imageData[i+2]);
-      pixels.push(imageData[i+3]);
+  for (let x = blockX; x < xEnd; x++) {
+    console.log('new row');
+    let rowCount = 0;
+    for (let y = blockY; y < yEnd; y++) {
+      rowCount++;
+      let i = x > 1 
+        ? (imageWidth * 4 * (x - 1)) + (y * 4) 
+        : (imageWidth * 4 * x) + (y * 4);
+      //console.log(rowCount, '-->', i);
+      // pixels.push(imageData[i]);
+      // pixels.push(imageData[i+1]);
+      // pixels.push(imageData[i+2]);
+      // pixels.push(imageData[i+3]);
     }
+    
   }
 
   saveBlockJpeg(pixels, blockSize);
@@ -66,7 +69,5 @@ saveBlockJpeg = function(pixels, bloxkSize){
   var jpegImageData = jpeg.encode(rawImageData, 50);
   fs.writeFileSync('c:\\temp\\image.jpg', jpegImageData.data);
 }
-
-
 
 module.exports.processImage = processImage;
