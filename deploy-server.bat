9ad74a60377a27c6921f95aa0f06e1dcb3ed4006
Christiaan Odendaal <christiaan@oden.co.za>
2023-08-10 11:12:24 +0200

    build script

--- deploy-server.bat
+++ deploy-server.bat
@@ -4,7 +4,7 @@ xcopy /Y "%~dp0\server\package.json" "E:\Development\BSC\Sem7\CM3070-Final Proje
 xcopy /E /S /Y "%~dp0\server\app\controllers" "E:\Development\BSC\Sem7\CM3070-Final Project\.SecureSight-deploy\server\app\controllers\"
 xcopy /E /S /Y "%~dp0\server\app\modules" "E:\Development\BSC\Sem7\CM3070-Final Project\.SecureSight-deploy\server\app\modules\"
 xcopy /E /S /Y "%~dp0\server\app\services" "E:\Development\BSC\Sem7\CM3070-Final Project\.SecureSight-deploy\server\app\services\"
-cd /d %~dp0
+cd /d %~dp0\server
 set /p build=< build.txt
 set /A build = build + 1
 echo %build% > build.txt
